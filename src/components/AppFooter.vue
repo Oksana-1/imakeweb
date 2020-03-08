099b33cd862dcb22b476cd69db2b7b74bcc7f2f7
oksana-1 <4438488@gmail.com>
2020-03-08 16:57:37 +0200

    Transfer old project fonts, svg and styles

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,60 @@
+<template>
+    <footer class="im-footer">
+        <div class="c-box-110 c-container im-footer-container">
+            <div class="im-common-subtitle">Contacts</div>
+            <div class="contact-row-wrap">
+                <div class="contact-row">
+                    <a href="mailto:belous.oksana@gmail.com" class="block-link"></a>
+                    <div class="contact-svg-wrap">
+                        <svg class="svgcontact">
+                            <use xlink:href="#email"></use>
+                        </svg>
+                    </div>
+                    <div class="contact-text">belous.oksana@gmail.com</div>
+                </div>
+            </div>
+            <div class="contact-row-wrap">
+                <div class="contact-row">
+                    <a href="tel:+380504438488" class="block-link"></a>
+                    <div class="contact-svg-wrap">
+                        <svg class="svgcontact">
+                            <use xlink:href="#tel"></use>
+                        </svg>
+                    </div>
+                    <div class="contact-text">tel: +380 504438488</div>
+                </div>
+            </div>
+            <div class="contact-row-wrap">
+                <div class="contact-row">
+                    <a target="_blank" href="https://www.linkedin.com/in/oksana-bilous/" class="block-link"></a>
+                    <div class="contact-svg-wrap">
+                        <svg class="svgcontact">
+                            <use xlink:href="#linkedin"></use>
+                        </svg>
+                    </div>
+                    <div class="contact-text">LinkedIn</div>
+                </div>
+            </div>
+        </div>
+        <div class="im-copyright">
+            <div class="c-box-110">
+                <p>&copy; Copyright 'I make web'.</p>
+            </div>
+        </div>
+        <SvgSprite/>
+    </footer>
+</template>
+
+<script>
+    import SvgSprite from './SvgSprite.vue';
+    export default {
+        name: "AppFooter.vue",
+        components: {
+            SvgSprite
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="im-header">
+        <div class="c-box-110">
+            <div class="im-header-wrap">
+                <div class="im-sandwich-wrap">
+                    <div class="im-sandwich-inner">
+                        <div class="im-sandwich"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader.vue"
+    }
+</script>
