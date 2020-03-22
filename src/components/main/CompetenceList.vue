40cce868482c2eec12a9176716acfb9153849456
oksana-1 <4438488@gmail.com>
2020-03-22 19:41:57 +0200

    Competencies section - WIP

--- src/components/main/CompetenceList.vue
+++ src/components/main/CompetenceList.vue
@@ -2,6 +2,44 @@
     <section class="im-common-section">
         <div class="im-container-1100">
             <div class="im-common-title dark centered">Competencies</div>
+            <div class="flex-row">
+                <div class="flex-33">
+                    <div class="logos-wrap margin-b-20">
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/html5_logo.svg" alt="HTML5">
+                        </div>
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/css3_logo.svg" alt="CSS3">
+                        </div>
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/js_logo.svg" alt="JS">
+                        </div>
+                    </div>
+                    <div class="im-common-subtitle centered">Solid basics</div>
+                </div>
+                <div class="flex-33">
+                    <div class="logos-wrap margin-b-20">
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/vue_logo.svg" alt="VUE">
+                        </div>
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/react_logo.svg" alt="REACT">
+                        </div>
+                    </div>
+                    <div class="im-common-subtitle centered margin-b-20">Modern frameworks</div>
+                </div>
+                <div class="flex-33">
+                    <div class="logos-wrap margin-b-20">
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/webpack_logo.svg" alt="WEBPACK">
+                        </div>
+                        <div class="side-logo-fix">
+                            <img class="fits" src="../../assets/images/github_logo_octocat.svg" alt="GITHUB">
+                        </div>
+                    </div>
+                    <div class="im-common-subtitle centered margin-b-20">Perfect tooling</div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
